e736f55b4520a277514dbad26ee23515c6dc52c5
Ahmad <balybinnintendo@gmail.com>
2022-01-07 21:11:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// // // // $('#text').on('click', () => {
+// // // //     console.log('Text clicked!!!')
+// // // // })
+
+// // // $('#text').click(() => {
+// // //     $('#text').hide(2000)
+// // // })
+
+// // // $('#button').click(() => {
+// // //     $('#text').show(2000)
+// // // })
+
+
+// // $('#button').click(() => {
+// //     $('#text').toggle()
+// // })
+
+
+// $('#text').css({
+//     background: 'red'
+// })
